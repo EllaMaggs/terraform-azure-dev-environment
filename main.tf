9d34ac14bcea2dd01bba881c2aaf1c2a0e2cb95f
Ella <ella.maggs@live.co.uk>
2025-01-19 10:47:02 +0000

    Deployed RG and VN

--- main.tf
+++ main.tf
@@ -9,4 +9,24 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "b5e7a8d2-4ced-4cf6-942e-9707cd426d93"
+}
+
+resource "azurerm_resource_group" "emtf-rm" {
+  name     = "emtf-resources"
+  location = "West Europe"
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_virtual_network" "emtf-vn" {
+  name                = "emtf-network"
+  resource_group_name = azurerm_resource_group.emtf-rm.name
+  location            = azurerm_resource_group.emtf-rm.location
+  address_space       = ["10.123.0.0/16"]
+
+  tags = {
+    environment = "dev"
+  }
 }
