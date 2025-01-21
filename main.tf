3285ca052adb759ab23ea76dde39c145268aed95
Ella <ella.maggs@live.co.uk>
2025-01-21 22:03:09 +0000

    Deployed subnet

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   subscription_id = "b5e7a8d2-4ced-4cf6-942e-9707cd426d93"
 }
 
-resource "azurerm_resource_group" "emtf-rm" {
+resource "azurerm_resource_group" "emtf-rg" {
   name     = "emtf-resources"
   location = "West Europe"
   tags = {
@@ -22,11 +22,18 @@ resource "azurerm_resource_group" "emtf-rm" {
 
 resource "azurerm_virtual_network" "emtf-vn" {
   name                = "emtf-network"
-  resource_group_name = azurerm_resource_group.emtf-rm.name
-  location            = azurerm_resource_group.emtf-rm.location
+  resource_group_name = azurerm_resource_group.emtf-rg.name
+  location            = azurerm_resource_group.emtf-rg.location
   address_space       = ["10.123.0.0/16"]
 
   tags = {
     environment = "dev"
   }
+}
+
+resource "azurerm_subnet" "emtf-subnet" {
+  name                 = "emtf-subnet"
+  resource_group_name  = azurerm_resource_group.emtf-rg.name
+  virtual_network_name = azurerm_virtual_network.emtf-vn.name
+  address_prefixes     = ["10.123.1.0/24"]
 }
