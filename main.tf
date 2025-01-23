de2fa0d1fc33a44aa0d2c5eb2bd63eca21a6f6dd
Ella <ella.maggs@live.co.uk>
2025-01-23 23:03:23 +0000

    Added SG rule and associated with subnet

--- main.tf
+++ main.tf
@@ -37,3 +37,32 @@ resource "azurerm_subnet" "emtf-subnet" {
   virtual_network_name = azurerm_virtual_network.emtf-vn.name
   address_prefixes     = ["10.123.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "emtf-sg" {
+  name                = "emtf-sg"
+  location            = azurerm_resource_group.emtf-rg.location
+  resource_group_name = azurerm_resource_group.emtf-rg.name
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "emtf-dev-rule" {
+  name                        = "emtf-dev-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "90.242.166.252/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.emtf-rg.name
+  network_security_group_name = azurerm_network_security_group.emtf-sg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "emtf-sga" {
+  subnet_id                 = azurerm_subnet.emtf-subnet.id
+  network_security_group_id = azurerm_network_security_group.emtf-sg.id
+}
